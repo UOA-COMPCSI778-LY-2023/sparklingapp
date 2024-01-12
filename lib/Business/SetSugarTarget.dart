b12c536b2083e4e1fd272281bd958bb12b473075
UoAres <syan408@aucklanduni.ac.nz>
2024-01-12 14:55:23 +1300

    adding SetSugarTarget class

--- lib/Business/SetSugarTarget.dart
+++ lib/Business/SetSugarTarget.dart
@@ -3,14 +3,15 @@ import 'package:dio/dio.dart';
 import '../components/LogUtils.dart';
 import '../interface/IHttpRequestHandler.dart';
 
-class GetProductInfoFromOpenFood implements IHttpRequestHandler {
+class SetSugarTarget implements IHttpRequestHandler {
   @override
   Future<Response> handleRequest(
       Dio dio, String url, Map<String, dynamic> parameters) async {
     DateTime beginTime = DateTime.now();
-    Response response = await dio.get(url, queryParameters: parameters);
+    // parameters = {"username": "mli178", "sugarTarget": 30}; //just for example based on the API document.
+    Response response = await dio.post(url, data: parameters);
     DateTime endTime = DateTime.now();
-    Log.instance.i("Get Request duration: ${endTime.difference(beginTime)}");
+    Log.instance.i("Post Request duration: ${endTime.difference(beginTime)}");
     return response;
   }
 }
